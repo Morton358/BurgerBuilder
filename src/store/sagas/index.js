eea5a581b933f976d6cd927e7ac2e45b9f668fff
Morton358 <vovankovalchuk358@gmail.com>
2018-03-24 23:57:47 +0100

    use yield all from redux-saga and call for some not asonchrynous functions

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,5 +1,4 @@
-import { takeEvery } from 'redux-saga/effects';
-import { fork } from 'redux-saga/effects';
+import { takeEvery, fork, all } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionsTypes';
 
@@ -21,10 +20,13 @@ export default function* root() {
 }
 
 function* watchAuth() {
-    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
-    yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
-    yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
-    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
+    yield all([
+        takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+        takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
+        takeEvery(actionTypes.AUTH_USER, authUserSaga),
+        takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+    ]);
+
 }
 
 function* watchBurgerBuilder() {
